4eade21039978e7e74aa2ab5c0efda9f4ad7ce8d
Bhaskar Rijal <bhaskarrijal13@gmail.com>
2023-03-29 20:29:22 +0545

    final

--- src/app/components/inprogress.tsx
+++ src/app/components/inprogress.tsx
@@ -3,26 +3,18 @@ import React from 'react'
 const Inprogress = () => {
     const inProgress = [
         {
-            name: 'Graphical DFA/NFA Simulator',
-            description: 'Web Application'
+            name: 'Weather API',
+            description: 'API Provider'
         },
         {
-            name: 'Noises of Nepal',
-            description: 'Web Application'
-        },
-        {
-            name: 'Block Network Organization',
-            description: 'Case Study'
-        },
-        {
-            name: 'Linear Steller Model',
+            name: '8085',
             description: 'Case Study'
         },
     ]
     return (
         <>
             <div className="flex flex-col gap-4">
-                <h2 className="font-medium text-2xl">Work in Progress</h2>
+                <h2 className="text-2xl font-medium">Work in Progress</h2>
                 <ul className="list-inside">
                     {
                         inProgress.map((project, index) => (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,15 +11,15 @@ export default function RootLayout({
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-				<title>Bhaskar Rijal</title>
-				<meta name="description" content="Bhaskar Rijal's personal website" />
-				<meta name="author" content="Bhaskar Rijal" />
+				<title>Kobin Neupane</title>
+				<meta name="description" content="Kobin Neupane's personal website" />
+				<meta name="author" content="Kobin Neupane" />
 
 				{/* OG */}
-				<meta property="og:title" content="Bhaskar Rijal" />
-				<meta property="og:description" content="Bhaskar Rijal's personal website" />
+				<meta property="og:title" content="Kobin Neupane" />
+				<meta property="og:description" content="Kobin Neupane's personal website" />
 				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://new.bhaskarrijal.me" />
+				<meta property="og:url" content="https://kobin.com.np" />
 				<meta property="og:image" content="/thumb.png" />
 
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
